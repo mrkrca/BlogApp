20fc3263b4d88d4eaa73b3b17933fa2183f3fb8d
mrkrca <aleksa.djuric.dj@gmail.com>
2024-05-15 23:16:27 +0200

    initial commit

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import ejs from "ejs";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app = express();
 const port = 3000;
